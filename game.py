4df4982cb598d2995607951434502ce5f6951767
Eduardo Moraes <edumoraes@live.com>
2021-03-05 16:29:20 -0300

    added game.py

--- game.py
+++ game.py
@@ -4,32 +4,50 @@ import random
 # Please read into the variables below the correct numbers. Use try and except to catch error.
 # a simple example would be:
 # hero_hp = int(input("how many hp does the hero have?"))
-hero_hp = 50
-dragon_hp = 100
-hero_max_dmg = 20
-dragon_max_dmg = 10
 
-print("The dragon with", dragon_hp, "hp attacks out hero with", hero_hp, "hp")
+try:
+    hero_hp = int(input("How many hp does the hero have? "))
+    dragon_hp = int(input("How many hp does the dragon have? "))
+    hero_max_dmg = int(input("What is the hero's maximum damage? "))
+    dragon_max_dmg = int(input("What is the dragon's maximum damage? "))
+except ValueError:
+    print("Please enter numbers only.")
+    exit()
+
+print("The dragon with", dragon_hp,
+      "hp attacks our hero with", hero_hp, "hp")
 
 # add a While for an infinite block
 # here goes the while:
+while True:
     dragon_attack = random.randint(1, dragon_max_dmg)
+
     # here you need to update the hero hp, you need to subtract the damage that the dragon did
     # add code on this line
-
+    hero_hp = hero_hp - dragon_attack
+    if hero_hp < 0:
+        hero_hp = 0
     print("The dragon does", dragon_attack, "damage and the hero has", hero_hp, "hp left")
+
     # add an if condition to check if the hero was killed by the dragon
     # here goes the if
+    if hero_hp <= 0:
         print("Unfortunately the dragon killed our hero. RIP sir Bravealot")
         break
 
     hero_attack = random.randint(1, hero_max_dmg)
+
     # here you need to update the dragon hp, you need to subtract the damage that the hero did
     # add code on this line
-
+    dragon_hp = dragon_hp - hero_attack
+    if dragon_hp < 0:
+        dragon_hp = 0
     print("The hero does", hero_attack, "damage and the dragon has", dragon_hp, "hp left")
+
     # add an if condition to check if the dragon was killed by the hero
     # here goes the if
+
+    if dragon_hp <= 0:
         print("Our valiant hero has slain the dragon!")
         break
 
